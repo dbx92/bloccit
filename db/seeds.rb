a25ddaa76ebae60ea148be89c82fecb198b336b7
dbx92 <davidpiobergamo@comcast.net>
2018-11-08 18:51:13 -0500

    seed_data_complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "random_data"
+
+50.times do
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+
+5.times do
+  Post.find_or_create_by!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+100.times do
+  Comment.create(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+10.times do
+  Comment.find_or_create_by(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
