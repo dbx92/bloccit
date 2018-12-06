f468d9bb1ceb9373b55edefb2c17556c4d1eda8d
dbx92 <davidpiobergamo@comcast.net>
2018-12-05 22:17:14 -0500

    crud-intro-complete

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @posts.each_with_index {|post, index| post.title = 'SPAM' if index % 5 === 0}
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
