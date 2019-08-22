96d9db55535ceca33decebb23c6a9e2d5dd67fea
dbx92 <davidpiobergamo@comcast.net>
2019-08-22 00:22:31 -0400

    posts_users_complete_2

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,9 +9,9 @@ class Post < ApplicationRecord
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 
   # def self.created_before(time)
-  #   where("created_at < ?", time)
+  #   where("created_at < ?", time) if time.present?
   # end
-  
+
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
   validates :topic, presence: true
